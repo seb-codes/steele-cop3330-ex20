7f3b84dd4281a346c9aa75faba5fece536107eb8
Sebastian <SebastianLSteele@knights.ucf.edu>
2021-06-06 20:45:25 -0400

    wow an inital commit

---
+++ src/main/java/org/example/MultiStateTaxCalculator.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MultiStateTaxCalculator {
+    public static void main(String[] args) {
+        double tax = 0.0;
+        Scanner intscan = new Scanner(System.in);
+        System.out.print("What is the order amount? ");
+        Integer order_amount = intscan.nextInt();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What state do you live in? ");
+        String state = scan.nextLine();
+        System.out.print("What county do you live in? ");
+        String county = scan.nextLine();
+
+
+        if(state.equals("Wisconsin")){
+            tax = 0.05;
+            if(county.equals("Eau Claire")){
+                tax+= 0.005;
+            }
+            else if(county.equals("Dunn")){
+                tax+=0.004;
+            }
+            else
+                System.out.println("No county tax 4 U");
+        }
+        else if(state.equals("Illinois")){
+            tax += 0.08;
+        }
+        else{
+            System.out.println("No state tax 4 U");
+        }
+
+        double tax_amount = tax * order_amount;
+        double total = tax_amount+ order_amount;
+
+        System.out.println(String.format("The tax is $%.02f\nThe total is $%.02f", tax_amount, total));
+
+
+
+
+    }
+}
